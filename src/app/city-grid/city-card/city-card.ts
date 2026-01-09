10f7fe3354392d54f79a4fa27091c1b9497708bf
Viarus <videomontagefactory@gmail.com>
2026-01-09 10:07:25 +0100

    getting the component structure

---
+++ src/app/city-grid/city-card/city-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-card',
+  imports: [],
+  templateUrl: './city-card.html',
+  styleUrl: './city-card.scss',
+})
+export class CityCard {
+
+}
---
+++ src/app/city-grid/city-grid.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-grid',
+  imports: [],
+  templateUrl: './city-grid.html',
+  styleUrl: './city-grid.scss',
+})
+export class CityGrid {
+
+}
